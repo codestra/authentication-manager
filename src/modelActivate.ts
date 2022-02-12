86ab961cae56d89352461c46da1cf23d2670453b
Sajad Ghawami <sajad.ghawami@codestra.io>
2022-02-12 20:48:26 +0100

    fixing error handling problem

--- src/modelActivate.ts
+++ src/modelActivate.ts
@@ -20,39 +20,35 @@ const modelActivate = async ({
   variables: { activationToken: string };
   onCompleted?: ({ token }: { token: string }) => void;
 }): Promise<string> => {
-  try {
-    const model = await Model.findOne({
-      activated: false,
-      activationToken,
-    });
+  const model = await Model.findOne({
+    activated: false,
+    activationToken,
+  });
 
-    if (!model) {
-      // TODO: Token expired error
-      throw new Error(ERROR_NOT_SPECIFIED);
-    }
-
-    await model.update({
-      activated: true,
-    });
-
-    const token = jwt.sign(
-      {
-        id: model._id,
-        email: model.email,
-      },
-      JWT_SECRET!,
-      {
-        expiresIn: JWT_EXPIRES!, // token will expire in 30days
-      },
-    );
-
-    if (onCompleted) {
-      onCompleted({ token });
-    }
-    return token;
-  } catch (error: any) {
-    throw new Error(error);
+  if (!model) {
+    // TODO: Token expired error
+    throw new Error(ERROR_NOT_SPECIFIED);
   }
+
+  await model.update({
+    activated: true,
+  });
+
+  const token = jwt.sign(
+    {
+      id: model._id,
+      email: model.email,
+    },
+    JWT_SECRET!,
+    {
+      expiresIn: JWT_EXPIRES!, // token will expire in 30days
+    },
+  );
+
+  if (onCompleted) {
+    onCompleted({ token });
+  }
+  return token;
 };
 
 export default modelActivate;
